fb224fb4a45e3fb38cbf35a33293d182ca0d6724
HaiPigGi <119752348+HaiPigGi@users.noreply.github.com>
2023-02-25 19:42:06 +0700

    isi File Backend

---
+++ HM-Backend/server.js
@@ -0,0 +1,103 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
+
+const express = require('express')
+const mysql=require('mysql')
+const BodyParser =require('body-parser')
+const passport=require('passport')
+const bcrypt= require('bcrypt')
+const app=express()
+const flash =require('express-flash')
+const session =require('express-session')
+const methodOverride = require('method-override')
+
+const users= []
+
+//for initialize
+const initializePassport = require('./passport-config')
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
+    const http=require("http")
+    const server=http.createServer(app)   
+//use app (ejs,express-flash,express-flash)
+app.set("view engine","ejs")
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+    secret: "key",
+    resave : false,
+    saveUninitialized: false
+}))
+//app use pass-initilize,pass-session, methodOverride
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name })
+  })
+
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+  })
+  
+  app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  }))
+  
+  app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+  })
+
+  //for get Data from front use methode post
+app.post("/register",async(req,res)=> {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password,10)
+        users.push({
+            id : Date.now().toString(),
+            name : req.body.name,
+            email : req.body.email,
+            password : hashedPassword
+        })
+        res.redirect("/login")
+    } catch  {
+        res.redirect("/register")
+    }
+})
+
+//for delete
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+  })
+  
+
+  //function for checking authenticated and redirect to login
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+  
+    res.redirect('/login')
+  }
+
+//function for checking not authenticated
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/')
+    }
+    next()
+  }
+
+app.listen(4000, () => {
+    console.log ("Server Ready");
+})
+
