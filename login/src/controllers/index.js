9c6698d23af4f0254832942d9248134cbe352e1c
HaiPigGi <119752348+HaiPigGi@users.noreply.github.com>
2023-03-02 19:52:48 +0700

    Login New

---
+++ login/src/controllers/index.js
@@ -0,0 +1,11 @@
+const login = require('./controller-login');
+const register = require('./controller-register');
+const home = require('./controller-home');
+const profile = require('./controller-kepengurusan');
+
+module.exports ={
+	login,
+	register,
+	home,
+	profile
+};
